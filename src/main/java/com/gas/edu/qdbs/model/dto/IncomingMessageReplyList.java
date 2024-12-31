242c9dca4489d7136d00fa5bab330e4242021546
N2-SGASPERI\s.gasperi <stefano.gasperi@gmail.com>
2024-12-31 12:17:00 +0100

    config lombok
    (cherry picked from commit 4e1769cee5af032029c50b8aa753dfa55172f388)

--- src/main/java/com/gas/edu/qdbs/model/dto/IncomingMessageReplyList.java
+++ src/main/java/com/gas/edu/qdbs/model/dto/IncomingMessageReplyList.java
@@ -2,9 +2,7 @@ package com.gas.edu.qdbs.model.dto;
 
 
 import com.gas.edu.qdbs.model.dto.top.ResponseListDTO;
-import lombok.*;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 
 @NoArgsConstructor
